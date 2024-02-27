ec3769b8af71a80618bf7d03880140c2b3d36fc5
liv-ya <150616599+liv-ya@users.noreply.github.com>
2024-02-27 15:54:14 +0530

    Add files via upload

---
+++ java/second.java
@@ -0,0 +1,20 @@
+class second{
+int rollno;
+static String name="Livya";
+String course;
+public static void main(String a[]){
+	second s=new second();
+	s.show(a);
+	System.out.println("You are in MACE");
+	
+}
+public static void show(String a[]){
+	System.out.println("My name is "+a[0]);
+	System.out.println("I'm a MCA student");
+
+}
+}
+
+
+
+
